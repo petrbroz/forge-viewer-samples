f304cfd6b03d986d8a64f5dfcce741e3efbbd5f4
Petr Broz <ipetrbroz@gmail.com>
2020-10-06 16:58:06 +0200

    Added serving of static assets.

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require('express');
 const { FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, PORT } = process.env;
 
 let app = express();
+app.use(express.static('public'));
 app.get('/api/auth/token', async function (req, res) {
     try {
         const authClient = new AuthenticationClient(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET);
         const token = await authClient.authenticate(['viewables:read']);
-        res.header('Access-Control-Allow-Origin', '*').json(token);
+        res.json(token);
     } catch(err) {
         console.error(err);
         res.status(500).send(err.message);
